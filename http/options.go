af76bdb14c3c6739b885e29be12d70f4739bf6c1
tangping <tangping@dustess.com>
2023-12-12 17:20:02 +0800

    调整默认http请求数为每秒100

--- http/options.go
+++ http/options.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/csrf"
@@ -22,7 +24,7 @@ func DefaultRuntimeOptions() RuntimeOption {
 	return RuntimeOption{
 		Middleware: []fiber.Handler{
 			cors.New(),
-			limiter.New(),
+			limiter.New(limiter.Config{Max: 100, Expiration: time.Second}),
 			csrf.New(),
 		},
 	}
